2e0632fc8527136c3e8ff808a1c398f473afc211
Rafli Ramadhani <rafliramadhani438@gmail.com>
2024-01-23 16:16:41 +0700

    testing component

---
+++ resources/views/livewire/navbar.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-red-white border border-b shadow-md w-full px-10 py-6 flex items-center justify-between">
+    <div>Navbar</div>
+    <div>About Me</div>
+    <div>Register</div>
+</div>
